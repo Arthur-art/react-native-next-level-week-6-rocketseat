e742ccaec00e702efe858d44767fd4b2f39f519a
Arthur-art <arthurteixeira.guts@gmail.com>
2021-07-26 15:38:26 -0300

    Adicionando img com component Image do react-native

--- gameplay/src/screens/signin/index.tsx
+++ gameplay/src/screens/signin/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react"
-import { View, StyleSheet, Text, TextInput } from "react-native"
+import { View, StyleSheet, Text, TextInput, Image } from "react-native"
 import { style } from "./styles";
+import ilustrationPng from "../../assets/illustration.png"
 
 export function Signin() {
     const [text, setText] = useState("")
     return (
         <View>
+            <Image source={ilustrationPng}/>
             <Text>{text}</Text>
             <TextInput onChangeText={(value) => setText(value)} style={style.input} />
         </View>
