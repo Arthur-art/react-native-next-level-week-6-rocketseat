ada78e7b829b31ca4d2cda0acfdb62fc654208b5
Arthur-art <arthurteixeira.guts@gmail.com>
2021-07-26 16:52:54 -0300

    Estilizando layout do button

--- gameplay/src/screens/ButtonIcon/style.ts
+++ gameplay/src/screens/ButtonIcon/style.ts
@@ -4,13 +4,19 @@ import { theme } from "../../../theme"
 export const style = StyleSheet.create({
     container: {
         width: 250,
-        height: 50,
+        height: 60,
         backgroundColor: theme.colors.primary,
         marginTop: 15,
+        borderRadius: 10,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center"
 
     },
     iconWrapper: {
-
+        borderRightWidth: 2,
+        padding: 15,
+        borderColor: theme.colors.line
     },
     image: {
 
