ecf9d01a79d5c6e7078fee8aec7f436f4b9414d4
Arthur-art <arthurteixeira.guts@gmail.com>
2021-08-02 11:18:45 -0300

    Trabalhando com expo/vector-icons para adicionar icon do button share, desenvolvendo background com component ImageBackground do react-native

--- gameplay/src/screens/AppointmentDetails/index.tsx
+++ gameplay/src/screens/AppointmentDetails/index.tsx
@@ -1,15 +1,36 @@
 import React from "react";
-import { View } from "react-native";
+import { ImageBackground, View, Text } from "react-native";
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
+import { Fontisto } from "@expo/vector-icons"
+import { BorderlessButton } from "react-native-gesture-handler";
+import { theme } from "../../../theme";
+import BannerPng from "../../assets/banner.png"
+import { style } from "./style"
 
 export function AppointmentsDetails() {
-
+    const { primary } = theme.colors
     return (
         <Background>
-            <Header title="Detalhes"
-
+            <Header
+                title="Detalhes"
+                action={
+                    <BorderlessButton>
+                        <Fontisto name="share" size={24} color={primary} />
+                    </BorderlessButton>
+                }
             />
+            <ImageBackground
+                source={BannerPng}
+                style={style.banner}
+            >
+
+                <Text style={style.title}>Lendários</Text>
+                <Text style={style.description}>
+                    Saga challenger do yodismo, do challenger ao bronze em 1 semana
+                </Text>
+
+            </ImageBackground>
         </Background>
     )
 }
--- gameplay/src/screens/AppointmentDetails/style.ts
+++ gameplay/src/screens/AppointmentDetails/style.ts
@@ -1,7 +1,20 @@
 import { StyleSheet } from "react-native";
-
+import { theme } from "../../../theme";
+const { heading } = theme.colors
 export const style = StyleSheet.create({
     container: {
 
+    },
+    banner: {
+        width: "100%",
+        height: 240
+    },
+    title: {
+        color: heading,
+        fontSize: 20
+    },
+    description: {
+        color: heading,
+        fontSize: 15
     }
 })
