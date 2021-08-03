fb7657f88ffcf91cdcbd3a1d137183b1404bd3e3
Arthur-art <arthurteixeira.guts@gmail.com>
2021-08-03 17:19:02 -0300

    Trabalhando com component react-native KeyboardAvoidingView e ScrollView

--- gameplay/src/screens/AppointmentCreate/index.tsx
+++ gameplay/src/screens/AppointmentCreate/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 import { Background } from "../../components/Background";
 import { CategorySelect } from "../../components/CategorySelect";
 import { Header } from "../../components/Header";
@@ -21,70 +21,76 @@ export function AppointmentsCreate() {
         categoryId === category ? setCategory('') : setCategory(categoryId)
     }
     return (
-        <Background>
-            <Header
-                title="Agendar partidas"
-            />
+        <KeyboardAvoidingView
+            style={style.container}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
 
-            <ListHeader title={"Categoria"} subtitle={""} />
+            <ScrollView>
+                <Header
+                    title="Agendar partidas"
+                />
 
-            <CategorySelect categorySelected={category} setCategory={handleCategorySelected} />
+                <ListHeader title={"Categoria"} subtitle={""} />
 
-            <View style={style.form}>
-                <RectButton>
-                    <View style={style.select}>
-                        {
-                            //   <View style={style.image} />
-                            <GuildIcon />
+                <CategorySelect categorySelected={category} setCategory={handleCategorySelected} />
 
-                        }
+                <View style={style.form}>
+                    <RectButton>
+                        <View style={style.select}>
+                            {
+                                //<View style={style.image} />
+                                <GuildIcon />
 
-                        <View style={style.selectBody}>
-                            <Text style={style.label}>Selecione um servidor</Text>
+                            }
+
+                            <View style={style.selectBody}>
+                                <Text style={style.label}>Selecione um servidor</Text>
+                            </View>
+
+                            <Feather
+                                name={"chevron-right"}
+                                color={theme.colors.heading}
+                                size={25}
+                            />
+                        </View>
+                    </RectButton>
+                    <View style={style.field}>
+
+                        <View>
+                            <Text style={style.label}>Dia e Mês</Text>
+                            <View style={style.column}>
+                                <SmallInput maxLength={2} />
+                                <Text style={style.divider}>
+                                    /
+                                </Text>
+                                <SmallInput maxLength={2} />
+                            </View>
                         </View>
 
-                        <Feather
-                            name={"chevron-right"}
-                            color={theme.colors.heading}
-                            size={25}
-                        />
-                    </View>
-                </RectButton>
-                <View style={style.field}>
-
-                    <View>
-                        <Text style={style.label}>Dia e Mês</Text>
-                        <View style={style.column}>
-                            <SmallInput maxLength={2} />
-                            <Text style={style.divider}>
-                                /
-                            </Text>
-                            <SmallInput maxLength={2} />
+                        <View>
+                            <Text style={style.label}>Horas e Minutos</Text>
+                            <View style={style.column}>
+                                <SmallInput maxLength={2} />
+                                <Text style={style.divider}>
+                                    :
+                                </Text>
+                                <SmallInput maxLength={2} />
+                            </View>
                         </View>
+
                     </View>
 
-                    <View>
-                        <Text style={style.label}>Horas e Minutos</Text>
-                        <View style={style.column}>
-                            <SmallInput maxLength={2} />
-                            <Text style={style.divider}>
-                                :
-                            </Text>
-                            <SmallInput maxLength={2} />
-                        </View>
+                    <View style={style.containerDescription}>
+                        <Text style={style.label}>Descrição</Text>
+
+                        <Text style={style.label}>Max 100 caracteres</Text>
                     </View>
+
+                    <TextArea multiline maxLength={100} numberOfLines={5} />
 
                 </View>
-
-                <View style={style.containerDescription}>
-                    <Text style={style.label}>Descrição</Text>
-                    
-                    <Text style={style.label}>Max 100 caracteres</Text>
-                </View>
-
-                <TextArea multiline maxLength={100} numberOfLines={5} />
-
-            </View>
-        </Background>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
